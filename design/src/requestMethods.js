731f41532441895cfc6f0382fbd2eb9308556e0b
Duncan Brewster <duncanwbrewster@gmail.com>
2022-01-03 12:57:01 -0800

    Switched from localhost to heroku api

--- design/src/requestMethods.js
+++ design/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://e-commerce-app-api.herokuapp.com/api/";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYjI1OWY5M2E5MDk0ZGIyZjM1MTQ1MCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzOTY5NDQwNSwiZXhwIjoxNjM5OTUzNjA1fQ.qQ50stAvNrxvKND908w4hBTKeF61bqKBH3ZReCJn6SE";
 
 export const publicRequest = axios.create({
